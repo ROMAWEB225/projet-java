13da6f5be8adccb9a01a4058e2565bb801118c23
ngorantanohromaric@gmail.com <ngorantanohromaric@gmail.com>
2025-01-09 21:04:51 +0100

    modification

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-let reponse = confirm("voulez vous un pain?");
-console.log(reponse);
-if (reponse == true){
-    console.log("Bon appetit");
-}
-else {
-    console.log("dommage");
+// let reponse = confirm("voulez vous un pain?");
+// console.log(reponse);
+// if (reponse == true){
+//     console.log("Bon appetit");
+// }
+// else {
+//     console.log("dommage");
+// }
+// FONCTION PROMPT sert a afficher une barre dans laquelle vous repondez a la question posée
+// let reponse = prompt("Entrez votre pays");
+// console.log(reponse);
+// let b = 0;
+// while(b < 3){
+//     console.log("Bonjour");
+//     console.log("Bonjour 2");
+//     console.log("Bonjour 3");
+//     console.log("Bonjour 4");
+//     b = b+1;
+// }
+// let b = 0;
+// while(b < 3){
+//     console.log(b);
+//     b = b + 1;
+// }
+// cas ou on prend 100 avec une condition avec le "while"
+// let b = 0;
+// while(b<100){
+//     if(b%2 == 0){
+//         console.log(b);
+//     }
+//     b = b+1;
+// }
+// let r = 23
+// while(r <= 533){
+//     console.log(r);
+//     r = r +1;
+// }
+// cas ou on utilise le "for" il se presente en reduisant le while en une ligne comme ceci
+// for( let i=23; i<534; i = i +1){
+//     console.log(i);
+// }
+
+for( let a = 0; a%8 ==0 && a<1000; a = a+1){
+    if(a%8 == 0){
+           console.log(a)
+    }
+    
 }
